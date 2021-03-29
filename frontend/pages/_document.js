7e0dc220b11a6873461a85afa50cbcbbcadb85b4
Young wook Jeon <ela87@naver.com>
2021-03-29 19:36:50 +0900

    Use nprogress module and its css

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -7,6 +7,7 @@ class MyDocument extends Document {
         <Head>
             <meta charSet="UTF-8" />
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossOrigin="anonymous"></link>
+            <link rel="stylesheet" href="/static/css/styles.css"/>
         </Head>
         <body>
           <Main />
