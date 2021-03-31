36e6fee36395f7a0f9f1152f5d5238f51d7a1155
Young wook Jeon <ela87@naver.com>
2021-03-31 22:35:03 +0900

    fetch all categories and tags

--- frontend/components/crud/BlogCreate.js
+++ frontend/components/crud/BlogCreate.js
@@ -8,6 +8,7 @@ import { getCategories } from "../../actions/category";
 import { getTags } from "../../actions/tag";
 import { createBlog } from "../../actions/blog";
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+import { QuillModules, QuillFormats } from "../../helpers/quill";
 import "../../node_modules/react-quill/dist/quill.snow.css";
 
 const CreateBlog = ({ router }) => {
@@ -22,6 +23,12 @@ const CreateBlog = ({ router }) => {
       return false;
     }
   };
+
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
+
+  const [checked, setChecked] = useState([]); // for categories checked
+  const [checkedTag, setCheckedTag] = useState([]);
 
   const [body, setBody] = useState(blogFormLS());
   const [values, setValues] = useState({
@@ -42,18 +49,57 @@ const CreateBlog = ({ router }) => {
     hidePublishButton,
   } = values;
 
+  const token = getCookie("token");
+
   useEffect(() => {
     setValues({ ...values, formData: new FormData() });
+    initCategories();
+    initTags();
   }, [router]);
+
+  const initCategories = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setCategories(data);
+      }
+    });
+  };
+
+  const initTags = () => {
+    getTags().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setTags(data);
+      }
+    });
+  };
 
   const publishBlog = (e) => {
     e.preventDefault();
+    createBlog(formData, token).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setValues({
+          ...values,
+          title: "",
+          error: "",
+          success: `새 블로그 "${data.title}"가 작성되었습니다`,
+        });
+        setBody("");
+        setCategories([]);
+        setTags([]);
+      }
+    });
   };
 
   const handleChange = (name) => (e) => {
     console.log(e.target.value);
     const value = name === "photo" ? e.target.files[0] : e.target.value;
-    formData.set(name, values);
+    formData.set(name, value);
     setValues({ ...values, [name]: value, formData, error: "" });
   };
 
@@ -65,10 +111,90 @@ const CreateBlog = ({ router }) => {
     }
   };
 
+  const handleToggle = (c) => () => {
+    setValues({ ...values, error: "" });
+    // return the first index or -1
+    const clickedCategory = checked.indexOf(c);
+    const all = [...checked];
+
+    if (clickedCategory === -1) {
+      all.push(c);
+    } else {
+      all.splice(clickedCategory, 1);
+    }
+    setChecked(all);
+    formData.set("categories", all);
+  };
+
+  const handleTagsToggle = (t) => () => {
+    setValues({ ...values, error: "" });
+    // return the first index or -1
+    const clickedTag = checkedTag.indexOf(t);
+    const all = [...checkedTag];
+
+    if (clickedTag === -1) {
+      all.push(t);
+    } else {
+      all.splice(clickedTag, 1);
+    }
+    setCheckedTag(all);
+    formData.set("tags", all);
+  };
+
+  const showCategories = () => {
+    return (
+      categories &&
+      categories.map((c, i) => (
+        <li className="list-unstyled" key={i}>
+          <input
+            onChange={handleToggle(c._id)}
+            type="checkbox"
+            className="m-1"
+          />
+          <label className="form-check-label">{c.name}</label>
+        </li>
+      ))
+    );
+  };
+
+  const showTags = () => {
+    return (
+      tags &&
+      tags.map((t, i) => (
+        <li className="list-unstyled" key={i}>
+          <input
+            onChange={handleTagsToggle(t._id)}
+            type="checkbox"
+            className="m-1"
+          />
+          <label className="form-check-label">{t.name}</label>
+        </li>
+      ))
+    );
+  };
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccess = () => (
+    <div
+      className="alert alert-success"
+      style={{ display: success ? "" : "none" }}
+    >
+      {success}
+    </div>
+  );
+
   const createBlogForm = () => {
     return (
       <form onSubmit={publishBlog}>
-        <div className="form-group">
+        <div className="form-group mb-2">
           <label className="text-muted">Title</label>
           <input
             type="text"
@@ -80,15 +206,15 @@ const CreateBlog = ({ router }) => {
 
         <div className="form-group">
           <ReactQuill
-            modules={CreateBlog.modules}
-            formats={CreateBlog.formats}
+            modules={QuillModules}
+            formats={QuillFormats}
             value={body}
             placeholder="당신의 글쓰기를 시작하세요."
             onChange={handleBody}
           />
         </div>
 
-        <div>
+        <div className="mt-2">
           <button type="submit" className="btn btn-primary">
             작성 완료
           </button>
@@ -97,36 +223,53 @@ const CreateBlog = ({ router }) => {
     );
   };
 
-  return <div>{createBlogForm()}</div>;
-};
+  return (
+    <div className="container-fluid pb-5">
+      <div className="row">
+        <div className="col-md-8">
+          {createBlogForm()}
+          <div className="pt-3">
+            {showError()}
+            {showSuccess()}
+          </div>
+        </div>
 
-CreateBlog.modules = {
-  toolbar: [
-    [{ header: "1" }, { header: "2" }, { header: [3, 4, 5, 6] }, { font: [] }],
-    [{ size: [] }],
-    ["bold", "italic", "underline", "strike", "blockquote"],
-    [{ list: "ordered" }, { list: "bullet" }],
-    ["link", "image", "video"],
-    ["clean"],
-    ["code-block"],
-  ],
-};
+        <div className="col-md-4">
+          <div>
+            <div className="form-group pb-2">
+              <h5>관련 이미지</h5>
+              <hr />
 
-CreateBlog.formats = [
-  "header",
-  "font",
-  "size",
-  "bold",
-  "italic",
-  "underline",
-  "strike",
-  "blockquote",
-  "list",
-  "bullet",
-  "link",
-  "image",
-  "video",
-  "code-block",
-];
+              <small className="text-muted">최대 사이즈: 1MB</small>
+              <label className="btn btn-outline-info">
+                관련 이미지 업로드
+                <input
+                  onChange={handleChange("photo")}
+                  type="file"
+                  accept="image/*"
+                  hidden
+                />
+              </label>
+            </div>
+          </div>
+          <div>
+            <h5>Categories</h5>
+            <hr />
+            <ul style={{ maxHeight: "200px", overflowY: "scroll" }}>
+              {showCategories()}
+            </ul>
+          </div>
+          <div>
+            <h5>Tags</h5>
+            <hr />
+            <ul style={{ maxHeight: "200px", overflowY: "scroll" }}>
+              {showTags()}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default withRouter(CreateBlog);
